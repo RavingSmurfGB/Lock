367850f8f5a73141869537b565b891567553749e
RavingSmurfGB <josephhutchinson4@gmail.com>
2020-06-03 17:35:08 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,11 @@
-!/usr/bin/env python
-
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 
 reader = SimpleMFRC522()
 
 try:
-        text = input('New data:')
-        print("Now place your tag to write")
-        reader.write(text)
-        print("Written")
+        id, text = reader.read()
+        print(id)
+        print(text)
 finally:
         GPIO.cleanup()
